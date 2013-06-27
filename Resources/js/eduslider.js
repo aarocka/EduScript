d29831305d5836ebf65f094e30ceb90921d67311
Thomas Lord <tmas@tgaragemedia.com>
2013-06-27 00:41:31 -0400

    oh shit it kind of works

--- Resources/js/eduslider.js
+++ Resources/js/eduslider.js
@@ -1,5 +1,17 @@
+var toggled = false;
 $(document).ready(function() {
 	$("#drawertrigger").click(function(event) {
-		alert("test");
+		if (!toggled) {
+			$("#eduslider").addClass("expanded");
+			$("#body").addClass("expanded");
+			$("#eduslider").removeClass("nexpanded");
+			$("#body").removeClass("nexpanded");
+		} else {
+			$("#eduslider").removeClass("expanded");
+			$("#body").removeClass("expanded");
+			$("#eduslider").addClass("nexpanded");
+			$("#body").addClass("nexpanded");
+		}
+		toggled = !toggled;
 	});
 });
